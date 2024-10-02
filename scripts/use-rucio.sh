53f641e4a67e9a41501fc4a3dac1c8f0eeb7f372
rdimaio <riccardodimaio11@gmail.com>
2024-10-02 11:23:42 -0700

    Rucio usage: change dir before starting client container

--- scripts/use-rucio.sh
+++ scripts/use-rucio.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+cd "$(dirname "$0")"
+
 echo "┌─────────────────────────────────────────────────────────────────┐"
 echo "⟾ kubectl: Rucio - Start client container pod for interactive use │"
 echo "└─────────────────────────────────────────────────────────────────┘"
